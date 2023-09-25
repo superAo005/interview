4227d4b3fe0ccad5dfd8146fa655defe05e9a1cd
superAo <810185149@qq.com>
2023-09-25 16:36:43 +0800

    feat:09-25

--- interview-js/14-继承.js
+++ interview-js/14-继承.js
@@ -37,6 +37,29 @@ let demo2 = new Son("TianTianUp", 20);
 demo1.colors.push("2"); // ["red", "blue", "green", "2"]
 demo2.colors.push("3"); // ["red", "blue", "green", "3"]
 
+// 构造函数
+function Person(name, age, gender) {
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.firends = ['zhangsan', 'lisi'];
+}
+Person.prototype = {
+//让实例的原型，指向Person构造函数
+  // constructor: Person, 
+  sayName: function () {
+    console.log(this.name);
+  }
+};
+let p1 = new Person('larry', 44, 'male');
+let p2 = new Person('terry', 39, 'male');
+
+p1.firends.push('robin');
+console.log(p1.firends); // [ 'zhangsan', 'lisi', 'robin' ]
+console.log(p2.firends); // [ 'zhangsan', 'lisi' ]
+console.log(p1.firends === p2.firends); // false
+console.log(p1.sayName === p2.sayName); // true
+
 // 使用Class
 
 class Rectangle {
