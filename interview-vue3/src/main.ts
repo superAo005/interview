0042cd3305de930eba4a7a30f951c1f341f783c1
superAo <810185149@qq.com>
2023-10-26 11:52:04 +0800

    feat:vue-element-admin

--- interview-vue3/src/main.ts
+++ interview-vue3/src/main.ts
@@ -1,14 +1,26 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "@/router";
+import { setupStore } from "@/store";
+import { setupDirective } from "@/directive";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import "@/permission";
 
-import App from './App.vue'
-import router from './router'
+// 本地SVG图标
+import "virtual:svg-icons-register";
 
-const app = createApp(App)
+// 国际化
+import i18n from "@/lang/index";
 
-app.use(createPinia())
-app.use(router)
+// 样式
+import "element-plus/theme-chalk/dark/css-vars.css";
+import "@/styles/index.scss";
+import "uno.css";
 
-app.mount('#app')
+const app = createApp(App);
+// 全局注册 自定义指令(directive)
+setupDirective(app);
+// 全局注册 状态管理(store)
+setupStore(app);
+
+app.use(router).use(i18n).mount("#app");
