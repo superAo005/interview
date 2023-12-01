54d91613e8681f855b55653f36c42a478541d646
yzw <zhenwang.yang@yintech.cn>
2023-12-01 13:21:37 +0800

    feat: 添加水印组件

---
+++ 4-interview-vue3/element-admin/src/views/demo/water.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box" ref="boxRef"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import { getMark } from "@/utils/waterMark";
+const boxRef = ref(null);
+const { waterMark } = getMark();
+onMounted(() => {
+  waterMark("PandaGuo", true, boxRef.value!); // 添加水印
+});
+</script>
+
+<style scoped>
+.box {
+  width: 500px;
+  height: 500px;
+  background-color: black;
+}
+</style>
