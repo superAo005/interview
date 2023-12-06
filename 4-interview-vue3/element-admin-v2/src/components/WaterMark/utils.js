57dbfd3ebe1183227eb3d9aad89d22a95e5dac93
yzw <zhenwang.yang@yintech.cn>
2023-12-06 14:20:29 +0800

    feat:水印

--- 4-interview-vue3/element-admin-v2/src/components/WaterMark/utils.js
+++ 4-interview-vue3/element-admin-v2/src/components/WaterMark/utils.js
@@ -1,41 +1,42 @@
-export const logoW = 35
-export const logoH = 25
-export const FontGap = 3
-export function toLowercaseSeparator (key) {
-  return key.replace(/([A-Z])/g, '-$1').toLowerCase()
+export const logoW = 35;
+export const logoH = 25;
+export const FontGap = 3;
+export function toLowercaseSeparator(key) {
+  return key.replace(/([A-Z])/g, "-$1").toLowerCase();
 }
-export function getStyleStr (style) {
+export function getStyleStr(style) {
   return Object.keys(style)
     .map((key) => `${toLowercaseSeparator(key)}: ${style[key]};`)
-    .join(' ')
+    .join(" ");
 }
 
-export function getPixelRatio () {
-  return window.devicePixelRatio || 1
+export function getPixelRatio() {
+  return window.devicePixelRatio || 1;
 }
+/** 是否重新渲染水印 */
 export const reRendering = (mutation, watermarkElement) => {
-  let flag = false
+  let flag = false;
   if (mutation.removedNodes.length && watermarkElement) {
-    flag = Array.from(mutation.removedNodes).includes(watermarkElement)
+    flag = Array.from(mutation.removedNodes).includes(watermarkElement);
   }
-  if (mutation.type === 'attributes' && mutation.target === watermarkElement) {
-    flag = true
+  if (mutation.type === "attributes" && mutation.target === watermarkElement) {
+    flag = true;
   }
-  return flag
-}
+  return flag;
+};
 
-function prepareCanvas (width, height, ratio = 1) {
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  const realWidth = width * ratio
-  const realHeight = height * ratio
-  canvas.setAttribute('width', `${realWidth}px`)
-  canvas.setAttribute('height', `${realHeight}px`)
-  ctx.save()
+function prepareCanvas(width, height, ratio = 1) {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  const realWidth = width * ratio;
+  const realHeight = height * ratio;
+  canvas.setAttribute("width", `${realWidth}px`);
+  canvas.setAttribute("height", `${realHeight}px`);
+  ctx.save();
 
-  return [ctx, canvas, realWidth, realHeight]
+  return [ctx, canvas, realWidth, realHeight];
 }
-export function getClips (
+export function getClips(
   content,
   logo,
   rotate,
@@ -50,15 +51,15 @@ export function getClips (
     width,
     height,
     ratio
-  )
-  const drawX = (gapX * ratio) / 2
-  const drawY = (gapY * ratio) / 2
+  );
+  const drawX = (gapX * ratio) / 2;
+  const drawY = (gapY * ratio) / 2;
   // const drawWidth = width * ratio
   // const drawHeight = height * ratio
 
-  const w = logo.width / 3 || logoW
-  const h = logo.height / 3 || logoH
-  ctx.drawImage(logo, drawX, drawY, w, h)
+  const w = logo.width / 3 || logoW;
+  const h = logo.height / 3 || logoH;
+  ctx.drawImage(logo, drawX, drawY, w, h);
   const {
     color,
     fontSize,
@@ -66,71 +67,71 @@ export function getClips (
     fontWeight,
     fontFamily,
     textAlign,
-    textBaseline
-  } = font
-  const mergedFontSize = Number(fontSize) * ratio
-  ctx.font = `${fontStyle} normal ${fontWeight} ${mergedFontSize}px/${height}px ${fontFamily}`
-  ctx.fillStyle = color
-  ctx.textAlign = textAlign
-  ctx.textBaseline = textBaseline
-  const contents = Array.isArray(content) ? content : [content]
+    textBaseline,
+  } = font;
+  const mergedFontSize = Number(fontSize) * ratio;
+  ctx.font = `${fontStyle} normal ${fontWeight} ${mergedFontSize}px/${height}px ${fontFamily}`;
+  ctx.fillStyle = color;
+  ctx.textAlign = textAlign;
+  ctx.textBaseline = textBaseline;
+  const contents = Array.isArray(content) ? content : [content];
   contents?.forEach((item, index) => {
     ctx.fillText(
-      item ?? '',
+      item ?? "",
       // contentWidth / 2,
       drawX + logoW + 10,
       index * (mergedFontSize + FontGap * ratio)
-    )
-  })
-  const angle = (Math.PI / 180) * Number(rotate)
-  const maxSize = Math.max(width, height)
-  const [rCtx, rCanvas, realMaxSize] = prepareCanvas(maxSize, maxSize, ratio)
+    );
+  });
+  const angle = (Math.PI / 180) * Number(rotate);
+  const maxSize = Math.max(width, height);
+  const [rCtx, rCanvas, realMaxSize] = prepareCanvas(maxSize, maxSize, ratio);
   // 从' ctx '复制并旋转
-  rCtx.translate(realMaxSize / 2, realMaxSize / 2)
-  rCtx.rotate(angle)
+  rCtx.translate(realMaxSize / 2, realMaxSize / 2);
+  rCtx.rotate(angle);
   if (contentWidth > 0 && contentHeight > 0) {
-    rCtx.drawImage(canvas, -contentWidth / 2, -contentHeight / 2)
+    rCtx.drawImage(canvas, -contentWidth / 2, -contentHeight / 2);
   }
   // 获取旋转文本的边界
-  function getRotatePos (x, y) {
-    const targetX = x * Math.cos(angle) - y * Math.sin(angle)
-    const targetY = x * Math.sin(angle) + y * Math.cos(angle)
-    return [targetX, targetY]
+  function getRotatePos(x, y) {
+    const targetX = x * Math.cos(angle) - y * Math.sin(angle);
+    const targetY = x * Math.sin(angle) + y * Math.cos(angle);
+    return [targetX, targetY];
   }
 
-  let left = 0
-  let right = 0
-  let top = 0
-  let bottom = 0
+  let left = 0;
+  let right = 0;
+  let top = 0;
+  let bottom = 0;
 
-  const halfWidth = contentWidth / 2
-  const halfHeight = contentHeight / 2
+  const halfWidth = contentWidth / 2;
+  const halfHeight = contentHeight / 2;
   const points = [
     [0 - halfWidth, 0 - halfHeight],
     [0 + halfWidth, 0 - halfHeight],
     [0 + halfWidth, 0 + halfHeight],
-    [0 - halfWidth, 0 + halfHeight]
-  ]
+    [0 - halfWidth, 0 + halfHeight],
+  ];
   points.forEach(([x, y]) => {
-    const [targetX, targetY] = getRotatePos(x, y)
-    left = Math.min(left, targetX)
-    right = Math.max(right, targetX)
-    top = Math.min(top, targetY)
-    bottom = Math.max(bottom, targetY)
-  })
+    const [targetX, targetY] = getRotatePos(x, y);
+    left = Math.min(left, targetX);
+    right = Math.max(right, targetX);
+    top = Math.min(top, targetY);
+    bottom = Math.max(bottom, targetY);
+  });
 
-  const cutLeft = left + realMaxSize / 2
-  const cutTop = top + realMaxSize / 2
-  const cutWidth = right - left
-  const cutHeight = bottom - top
-  const realGapX = gapX * ratio
-  const realGapY = gapY * ratio
-  const filledWidth = (cutWidth + realGapX) * 2
-  const filledHeight = cutHeight + realGapY
+  const cutLeft = left + realMaxSize / 2;
+  const cutTop = top + realMaxSize / 2;
+  const cutWidth = right - left;
+  const cutHeight = bottom - top;
+  const realGapX = gapX * ratio;
+  const realGapY = gapY * ratio;
+  const filledWidth = (cutWidth + realGapX) * 2;
+  const filledHeight = cutHeight + realGapY;
 
-  const [fCtx, fCanvas] = prepareCanvas(filledWidth, filledHeight)
+  const [fCtx, fCanvas] = prepareCanvas(filledWidth, filledHeight);
 
-  function drawImg (targetX = 0, targetY = 0) {
+  function drawImg(targetX = 0, targetY = 0) {
     fCtx.drawImage(
       rCanvas,
       cutLeft,
@@ -141,11 +142,17 @@ export function getClips (
       targetY,
       cutWidth,
       cutHeight
-    )
+    );
   }
-  drawImg()
-  drawImg(cutWidth + realGapX, -cutHeight / 2 - realGapY / 2)
-  drawImg(cutWidth + realGapX, +cutHeight / 2 + realGapY / 2)
+  drawImg();
+  drawImg(cutWidth + realGapX, -cutHeight / 2 - realGapY / 2);
+  drawImg(cutWidth + realGapX, +cutHeight / 2 + realGapY / 2);
 
-  return [fCanvas.toDataURL(), filledWidth / ratio, filledHeight / ratio]
+  return [fCanvas.toDataURL(), filledWidth / ratio, filledHeight / ratio];
+}
+/** 以水印为中心点旋转*/
+export function rotateWatermark(ctx, rotateX, rotateY, rotate) {
+  ctx.translate(rotateX, rotateY);
+  ctx.rotate((Math.PI / 180) * Number(rotate));
+  ctx.translate(-rotateX, -rotateY);
 }
