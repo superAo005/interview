6cc2035b4222f90ecda2a0d11bff3f81c81974f5
yzw <zhenwang.yang@yintech.cn>
2024-01-03 09:50:12 +0800

    feat:优化

--- 4-interview-vue3/element-admin-vite/src/layouts/layout/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AppMain } from "./AppMain.vue"
-export { default as HeaderNav } from "./HeaderNav/index.vue"
-export { default as HeaderRight } from "./HeaderRight/index.vue"
-export { default as TopBar } from "./TopBar/index.vue"
