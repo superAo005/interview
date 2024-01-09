ef84bf0a3edf0260761291ba326850d6aa33108c
yzw <zhenwang.yang@yintech.cn>
2024-01-09 16:21:06 +0800

    feat:模版

---
+++ 9f/Template/index.vue
@@ -0,0 +1,12 @@
+<!--
+* @Author: yzw
+* @Date: 2024-01-04
+* @LastEditTime: 2024-01-04
+* @LastEditors: yzw
+* @Description:新建主题合集
+ -->
+<template>
+  <div></div>
+</template>
+<script setup lan></script>
+<style scoped lang="scss"></style>
