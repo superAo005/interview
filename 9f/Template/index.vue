fadd0ea66ac76798dbc238dbf333c8634d1d63e0
yzw <zhenwang.yang@yintech.cn>
2024-01-22 10:05:40 +0800

    feat:vue-admin

--- 9f/Template/index.vue
+++ 9f/Template/index.vue
@@ -9,10 +9,30 @@
   <div class="app-contaier"></div>
 </template>
 <script setup lang="ts">
-import { ref, Ref } from "vue";
-defineProps<{ msg: string }>();
-defineEmits(["update"]);
-const ref: Ref<HTMLElement | null> = ref(null);
+import { computed, ref, shallowRef } from "vue";
+import Dialog from "./Dialog.vue";
+interface Props {
+  /** 控制 modal 显隐 */
+  modelValue: boolean;
+}
+// const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: false,
+});
+const emit = defineEmits<{
+  "update:modelValue": [boolean];
+}>();
+const inputRef = ref<HTMLInputElement | null>(null);
+const scrollbarRef = ref<InstanceType<typeof ElScrollbar> | null>(null);
+const searchResultRef = ref<InstanceType<typeof Dialog> | null>(null);
+const modalVisible = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(value: boolean) {
+    emit("update:modelValue", value);
+  },
+});
 </script>
 <style scoped lang="scss">
 .app-contaier {
